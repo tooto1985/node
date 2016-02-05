fcc3eee1d854fbef866743a5e6dba6ac57d69f93
Tommy <tooto1985@gmail.com>
2016-02-05 10:44:42 +0800

    dev 2

--- complete/7-11/app.js
+++ complete/7-11/app.js
@@ -1,18 +1,12 @@
 var express = require("express");
 var path = require("path");
-var fs = require("fs");
-var acceptLanguage = require("accept-language");
 var app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function(req, res) {
-    var language = acceptLanguage.parse(req.headers["accept-language"]);
-    var folder = language[0].language;
-    fs.stat("./public/" + folder, function(err, stats) {
-    	if (!err && stats.isDirectory()) {
-    		res.redirect(folder);
-        } else {
-            res.redirect("en");
-        }
-    });
+    if (req.headers["accept-language"].split(",")[0] === "zh-TW") {
+        res.redirect("zh");
+    } else {
+        res.redirect("en");
+    }
 });
 app.listen(process.env.PORT || 3000);
--- exercise/7-11/app.js
+++ exercise/7-11/app.js
@@ -1,7 +1,5 @@
 var express = require("express");
 var path = require("path");
-var fs = require("fs");
-
 var app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function(req, res) {
@@ -10,9 +8,6 @@ app.get("/", function(req, res) {
 
 
 
-
-
-
-
 });
 app.listen(process.env.PORT || 3000);
+
