59de9574ad8c0300d073aba1e384e52646cf8744
Tommy <tooto1985@gmail.com>
2016-02-07 00:25:01 +0800

    dev 7

---
+++ complete/6-3(new)/api.js
@@ -0,0 +1,15 @@
+var util = require("util");
+var url = require("url");
+var number;
+module.exports = function(request, response) {
+	var pathname = url.parse(request.url).pathname;
+    var query = url.parse(request.url,true).query;
+    if (pathname === "/api/") {
+        response.writeHead({
+            "Content-Type":"application/json"
+        });
+        response.write(JSON.stringify(["John","Mark"]));
+        response.end();
+    }
+    console.log(util.inspect(query) + " in " + pathname);
+}
