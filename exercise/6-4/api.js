560bad160b55c9eab219d78e992f50717ae88702
Tommy <tooto1985@gmail.com>
2016-02-08 16:24:25 +0800

    dev 14

---
+++ exercise/6-4/api.js
@@ -0,0 +1,27 @@
+var url = require("url");
+
+module.exports = function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var query = url.parse(request.url, true).query;
+    if (pathname === "/api/") {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
