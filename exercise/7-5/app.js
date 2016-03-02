0c1e0d61978e2eb8ce6ba7ec45bea1ebef8f1f35
Tommy <tooto1985@gmail.com>
2016-03-02 22:58:11 +0800

    test 3

--- exercise/7-5/app.js
+++ exercise/7-5/app.js
@@ -1,22 +1,6 @@
-var http = require("http");
 var express = require("express");
 var app = express();
-app.get("/", function(req, res) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/",function(req, res) {
+    
 });
 app.listen(process.env.PORT || 3000);
---
+++ exercise/7-6/app2.js
@@ -0,0 +1,10 @@
+
+var express = require("express");
+var app = express();
+app.get("/:area", function(req, res) {
+    
+        
+
+
+});
+app.listen(3000);
