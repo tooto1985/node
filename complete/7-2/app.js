3aa3312559571105f2a349e82875041e46a0d996
Tommy <tooto1985@gmail.com>
2016-03-02 17:54:24 +0800

    dev 25

--- complete/7-2/app.js
+++ complete/7-2/app.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 app.get("/", function(req, res) {
-    res.send("<h1>Node.JS!</h1>");
+    res.send("<h1>你好Node.JS!</h1>");
 });
 app.get("/text", function(req, res) {
     res.set("Content-Type", "text/plain");
-    res.send("<h1>Node.JS!</h1>");
+    res.send("<h1>你好Node.JS!</h1>");
 });
 app.listen(process.env.PORT || 3000);
--- exercise/7-3/app.js
+++ exercise/7-3/app.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var app = express();
 app.get("/", function(req, res) {
-    res.send("<h1>Node.JS!</h1>");
+    res.send("<h1>你好Node.JS!</h1>");
 });
 app.get("/text", function(req, res) {
     res.set("Content-Type", "text/plain");
-    res.send("<h1>Node.JS!</h1>");
+    res.send("<h1>你好Node.JS!</h1>");
 });
 
 
