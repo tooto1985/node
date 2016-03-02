7a4ccc173dfb186e8c5a412f78819c683fe9a3ef
Tommy <tooto1985@gmail.com>
2016-03-02 21:59:08 +0800

    test 2

--- complete/7-5/app2.js
+++ complete/7-5/app2.js
@@ -1,11 +1,10 @@
 var jsdom = require("jsdom");
 var express = require("express");
 var app = express();
-app.get("/", function(req, res) {
+app.get("/:area", function(req, res) {
     jsdom.env("http://www.cwb.gov.tw/V7/forecast/week/week.htm", ["http://code.jquery.com/jquery.js"], function(err, window) {
         var $ = window.$;
-        console.log($(".BoxTableInside").html());
-        res.end("ok");
+        res.send("<table><tr>" + $("th:contains('" + req.params.area + "')").parent().html() + "</tr></table>");
     });
 });
 app.listen(3000);
