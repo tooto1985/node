78bbde14176d6a9e3061a64dbf19db67cba01ee0
Tommy <tooto1985@gmail.com>
2016-03-02 23:27:43 +0800

    dev 27

--- exercise/7-7/app.js
+++ exercise/7-7/app.js
@@ -5,7 +5,4 @@ var app = express();
 app.get("/", function(req, res) {
     res.send("你的名子是:" + req.query.name);
 });
-app.post("/", function(req, res) {
-    
-});
 app.listen(process.env.PORT || 3000);
--- exercise/7-8/app.js
+++ exercise/7-8/app.js
@@ -1,19 +1,12 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 
-
+var path = require("path");
 var app = express();
 
-
-
-app.use(bodyParser());
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function(req, res) {
     res.send("你的名子是:" + req.query.name);
 });
-
-
-
-
 
 
 
