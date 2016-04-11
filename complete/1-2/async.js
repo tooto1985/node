f2f70be3d8428e55c721a98f98789987e009d921
tommy <tooto1985@gmail.com>
2016-04-11 22:54:55 +0800

    dev 40

--- complete/1-2/async.js
+++ complete/1-2/async.js
@@ -1,5 +1,6 @@
-console.log("1");
-setTimeout(function run() {
-    console.log("4");
-}.bind(console.log("2")),0);
-console.log("3");
+console.log(1);
+setTimeout(run.bind(console.log(2)));
+function run() {
+    console.log(3);
+}
+console.log(4);
--- complete/1-2/sync.js
+++ complete/1-2/sync.js
@@ -1,6 +1,6 @@
-console.log("1");
+console.log(1);
+run(console.log(2));
 function run() {
-    console.log("3");
+    console.log(3);
 }
-run(console.log("2"));
-console.log("4");
+console.log(4);
