ce0efc0432a0f7a45abeab31937a452697836ec3
Tommy <tooto1985@gmail.com>
2016-09-05 14:20:31 +0800

    test 19

--- complete/8-1/api.js
+++ complete/8-1/api.js
@@ -8,13 +8,17 @@ module.exports = function(request, response) {
         response.writeHead(200,{
             "Content-Type":"application/json; charset=utf-8"
         });
-        var tips=[
-            "隨機顯示提示訊息",
-            "隨時可點選開啟提示",
-            "滑鼠滑入照片顯示說明文字"
-        ];
-        var tip = tips[~~(Math.random()*tips.length)]; //Math.floor
-        response.write(JSON.stringify(tip));
+        if (!cookies.get("isRead")) {
+            var tips = [
+                "隨機顯示提示訊息",
+                "隨時可點選開啟提示",
+                "滑鼠滑入照片顯示說明文字"
+            ];
+            var tip = tips[~~(Math.random() * tips.length)]; //Math.floor
+            response.write(JSON.stringify(tip));
+        } else {
+            response.write(JSON.stringify(null));
+        }
         response.end();
     }
 };
