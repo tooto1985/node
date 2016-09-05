e32eea5d940556415db5b23ba60c466ca107a607
Tommy <tooto1985@gmail.com>
2016-09-05 16:17:34 +0800

    test 20

--- exercise/8-3/app.js
+++ exercise/8-3/app.js
@@ -1,20 +1,11 @@
 var express = require("express");
-
+var path = require("path");
 var app = express();
 var cookieParser = require("cookie-parser");
 app.use(cookieParser());
-
-app.get("/", function(req, res) {
-    var myname = "";
-    if (req.query.myname) {
-        res.cookie("myname", req.query.myname);
-        myname = req.query.myname;
-    } else {
-        if (req.cookies.myname) {
-            myname = req.cookies.myname;
-        }
-    }
-    res.send("myname=" + myname);
-
+app.use(function(req, res, next) {
+    console.log(req.cookies);
+    next();
 });
+app.use(express.static(path.join(__dirname, "public")));
 app.listen(process.env.PORT || 3000);
