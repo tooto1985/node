abaf376393be7786fd4acddd3e92aa0a2c7d3f0e
tommy <tooto1985@gmail.com>
2017-08-20 14:23:09 +0800

    test 69

---
+++ complete/4-12/app.js
@@ -0,0 +1,56 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var mime = require("mime");
+var crypto = require("crypto");
+http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    if (pathname.substr(pathname.length - 1) === "/") {
+        pathname += "index.html"; //若無帶入檔名預設為index.html
+    } else {
+        var paths = pathname.split("/").pop();
+        if (paths.indexOf(".") === -1) {
+            response.writeHead(301, {
+                "Location": pathname + "/" + (url.parse(request.url).search || "")
+            });
+            response.end();
+            return;
+        }
+    }
+    pathname = (process.argv[2] || ".") + pathname; //若有傳入參數則使用參數的路徑
+    pathname = decodeURI(pathname);
+    fs.stat(pathname, function (err, stats) {
+        if (!err) {
+            if (stats.isFile()) {
+                fs.readFile(pathname, function (err, data) {
+                    if (!err) {
+                        var hash = crypto.createHash('sha1').update(data).digest('base64');
+                        if (request.headers['if-none-match'] == hash) {
+                            response.writeHead(304);
+                            response.end();
+                            return;
+                        }
+
+                        response.writeHead(200, {
+                            "Content-Type": mime.lookup(pathname),
+                            "Etag": hash
+                        });
+                        response.write(data);
+                    } else {
+                        console.log(err);
+                    }
+                    response.end();
+                });
+            } else {
+                notFound();
+            }
+        } else {
+            notFound();
+        }
+    });
+    function notFound() {
+        response.writeHead(404);
+        response.write("404 not found");
+        response.end();
+    }
+}).listen(process.env.PORT || 3000);
